adf569153a207111d354c9f346c2a46b014f3e27
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-14 17:11:33 +0300

    comment Checkout utils

--- jenkins/api_pipeline.groovy
+++ jenkins/api_pipeline.groovy
@@ -7,14 +7,14 @@ timeout(time: 30, unit: 'MINUTES') {
             checkout scm
         }
 
-        stage('Checkout utils') {
-            dir('utils') {
-                git branch: 'main', url: 'https://github.com/cristina-dautova/otus-test-runner.git', credentialId: 'jenkins'
-            }
-        }
-
-        utils = load './tools/utils'
-        utils.prepare_yaml_config
+//        stage('Checkout utils') {
+//            dir('utils') {
+//                git branch: 'main', url: 'https://github.com/cristina-dautova/otus-test-runner.git', credentialId: 'jenkins'
+//            }
+//        }
+//
+//        utils = load './tools/utils'
+//        utils.prepare_yaml_config
 
         stage('Running UI tests') {
 
