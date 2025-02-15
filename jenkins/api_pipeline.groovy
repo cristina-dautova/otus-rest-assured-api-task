83433c1df3ec4db78ecf5861e1688ed7ef3b491c
cristina-dautova <70657896+cristina-dautova@users.noreply.github.com>
2025-02-15 18:21:14 +0300

    Update api_pipeline.groovy

--- jenkins/api_pipeline.groovy
+++ jenkins/api_pipeline.groovy
@@ -30,7 +30,7 @@ timeout(time: 30, unit: 'MINUTES') {
         }
 
         stage('Verify Allure Results') {
-                sh 'ls -la target/allure-results || true'
+                sh 'ls -la build/allure-results || true'
         }
 
         stage('Publish allure report') {
